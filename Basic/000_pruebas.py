bf653e6f6bbb80d0317ccebe67713f92f052d805
Nazareno Lozano <estudiotecrl@gmail.com>
2024-01-18 12:38:29 -0300

    ya avanzamos hasta set, segui estudiando

---
+++ Basic/000_pruebas.py
@@ -0,0 +1,19 @@
+hola = (1,2,3,4,5, "Naza", "Lozano", "casa")
+print(hola)
+print(type(hola))
+
+hola1 = 10
+hola2 = "diez"
+print(hola1,hola2)
+print(type(hola1,))
+print(type(hola2,))
+
+hola3 = [1,2,3]
+print(type(hola3))
+
+hola4 = { #esto es rariimo no se que es u SET a leer
+    "nombre" , "nazareno",
+    "apellido" , "lozano",
+}
+print(hola4)
+print(type(hola4))
--- Basic/03_strings.py
+++ Basic/03_strings.py
@@ -9,13 +9,13 @@ print(len(my_string))
 print(len(my_other_string))
 print(my_string + " " + my_other_string)
 
-my_new_line_string = "Este es un String\ncon salto de línea"
+my_new_line_string = "Este es un String \n con salto de línea"
 print(my_new_line_string)
 
 my_tab_string = "\tEste es un String con tabulación"
 print(my_tab_string)
 
-my_scape_string = "\\tEste es un String \\n escapado"
+my_scape_string = "\tEste es un String \n escapado"
 print(my_scape_string)
 
 # Formateo
